f291587300db5dad618f77ac12fece9e418a589f
Vladislav Yeremeyev <vladislav.eremeev@uchi.ru>
2020-05-19 19:38:37 +0300

    added Header and ShoppingCartSidebar components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import ShoppingCartSidebar from "../ShoppingCartSidebar/ShoppingCartSidebar";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "left",
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {/*<IconButton*/}
+          {/*  edge="start"*/}
+          {/*  className={classes.menuButton}*/}
+          {/*  color="inherit"*/}
+          {/*  aria-label="menu"*/}
+          {/*>*/}
+          {/*  <MenuIcon />*/}
+          {/*</IconButton>*/}
+          <Typography variant="h6" className={classes.title}>
+            Items List
+          </Typography>
+          <IconButton onClick={() => {}}>
+            <ShoppingCartIcon style={{ color: "white" }} />
+          </IconButton>
+          <ShoppingCartSidebar />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
